7ce5a7f57e8161deb48b6b1daf9035d5c8df1df0
il-y-seto <y-seto@infiniteloop.com>
2020-03-22 04:34:45 +0900

    nuxt移行に伴うデザイン崩れを修正

--- pages/index.vue
+++ pages/index.vue
@@ -51,6 +51,12 @@
   </v-container>
 </template>
 
+<style>
+.container {
+  font-size: 12px;
+}
+</style>
+
 <script>
 import Vue from 'vue'
 import axios from 'axios'
@@ -144,36 +150,3 @@ export default Vue.extend({
   }
 })
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
