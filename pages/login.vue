bbc568513bc8d5b6347cc78f2abbaa63583a7c2b
il-y-seto <y-seto@infiniteloop.com>
2021-03-16 04:10:44 +0900

    add: ログインページをガワだけ作成

---
+++ pages/login.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container>
+    <v-form @submit.prevent="login">
+      <v-text-field v-model="email" placeholder="email" name="email" />
+      <v-text-field v-model="password" placeholder="password" name="password" />
+      <v-btn type="submit">ログイン</v-btn>
+    </v-form>
+    <v-form @submit.prevent="externalLogin('twitter')">
+      <v-btn type="submit">twitter</v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator"
+import axios from "axios"
+
+
+@Component({
+})
+export default class LoginPage extends Vue {
+  public email: string = ""
+  public password: string = ""
+
+  public async login(): Promise<void> {
+    await axios
+      .get(`/hoge`)
+      .then((res) => {
+        console.log(res)
+      })
+  }
+
+  public async externalLogin(service: string): Promise<void> {
+    if (service === "twitter") {
+      this.$router.push("/oauth/redirect")
+    }
+  }
+}
+</script>
---
+++ pages/oauth/redirect.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    リダイレクトしています
+    <v-form @submit.prevent="redirect">
+      <v-btn type="submit">twitter</v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator"
+import { Context } from "@nuxt/types"
+
+
+@Component({
+})
+export default class OauthRedirectPage extends Vue {
+  public twitterAuthUrl: string = ''
+
+  public redirect() {
+    this.$axios.$get('http://localhost/api/test')
+      .then(data => {
+        return { twitterAuthUrl: data.redirect_url }
+      }).catch(e => console.error({ message: e.message, statusCode: e.statusCode }))
+  }
+}
+</script>
