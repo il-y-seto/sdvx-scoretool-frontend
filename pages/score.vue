7543d9da850be9ace111312a6d79552a244430bd
il-y-seto <y-seto@infiniteloop.com>
2021-06-22 09:26:17 +0900

    add: 自身のスコア一覧ページ(仮)作成

---
+++ pages/score.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-data-table
+      :headers="headers"
+      :items="scores"
+    />
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Context } from "@nuxt/types"
+import { Component, Vue } from "nuxt-property-decorator"
+
+@Component
+export default class ScorePage extends Vue {
+  public scores: any = []
+  public async asyncData(context: Context) {
+    // ssr時はコンテナ間通信になってめんどくさい
+    const scores = await context.$axios.get(`http://laravel.test/api/user-score`)
+      .then((res) => {
+        console.log(res.data.scores)
+        return res.data.scores
+      })
+    return {
+      scores,
+    }
+  }
+
+  public headers: Array<object> = [
+    { text: "name", value: "name"},
+    { text: "difficulty", value: "difficulty"},
+    { text: "level", value: "level"},
+    { text: "clear_lump", value: "clear_lump"},
+    { text: "score", value: "score"},
+  ]
+}
+</script>
